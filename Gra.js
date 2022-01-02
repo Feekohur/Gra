72e946873cb43e7ae9f74faf90dd55ed30385c99
Michał Król <77741@student.pb.edu.pl>
2022-01-02 14:34:41 +0100

    Teraz powinno wyświetlać wszystko i się poruszać

--- Gra.js
+++ Gra.js
@@ -17,14 +17,8 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('player', 
-        'ball.png',
-        { frameWidth: 20, frameHeight: 20 }
-    );
-    this.load.image('platform', 
-        'mario-brick.png',
-        { frameWidth: 20, frameHeight: 20 }
-    );
+    this.load.image('player', 'Mario.png');
+    this.load.image('platform', 'platform.png');
 }
 let player;
 let cursors;
@@ -35,10 +29,11 @@ function create ()
     cursors = this.input.keyboard.createCursorKeys();
     platforms = this.physics.add.staticGroup();
     platforms.create(600, 550, 'platform');
-    platforms.create(500, 500, 'platform');
-    platforms.create(400, 450, 'platform');
-    platforms.create(150, 375, 'platform');
+    platforms.create(150, 400, 'platform');
+    platforms.create(600, 250, 'platform');
+    platforms.create(150, 100, 'platform');
     player = this.physics.add.sprite(20, 600, 'player');
+    player.setScale(0.2);
     player.setCollideWorldBounds(true);
     player.body.gravity.y = 500;
     this.physics.add.collider(player, platforms);
@@ -52,15 +47,15 @@ function update() {
         player.setVelocityX(300);
     }
     else if(cursors.up.isDown && player.body.onFloor()){
-        player.setVelocityY(-300);
+        player.setVelocityY(-450);
     }
     else if(cursors.left.isDown && cursors.up.isDown && player.body.onFloor()){
         player.setVelocityX(-300);
-        player.setVelocityY(-300);
+        player.setVelocityY(-450);
     }
     else if(cursors.right.isDown && cursors.up.isDown && player.body.onFloor()){
         player.setVelocityX(300);
-        player.setVelocityY(-300);
+        player.setVelocityY(-450);
     }
     else{
         player.setVelocityX(0);
