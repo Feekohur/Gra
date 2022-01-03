64090287b1beb0ac98ce1c93df1955f34221395b
Krystian <krystian.szmigielski2@gmail.com>
2022-01-03 18:22:47 +0100

    Moved files to folders, added destroying breakable blocks

--- Gra.js
+++ Gra.js
@@ -23,14 +23,15 @@ let jumpForce = 270
 
 function preload ()
 {
-    this.load.atlas('mario', 'mario.png', 'mario.json')
-    this.load.tilemapTiledJSON('map', 'mapa.json');
-    this.load.image('bricks-spritesheet', 'bricks-spritesheet.png');  
+    this.load.atlas('mario', 'mario/mario.png', 'mario/mario.json')
+    this.load.tilemapTiledJSON('map', 'map/mapa.json');
+    this.load.image('bricks-spritesheet', 'map/bricks-spritesheet.png');  
 }
 
 //let player;
 let cursors;
 let mario;
+let map;
 
 function create ()
 {
@@ -41,16 +42,15 @@ function create ()
     player.body.gravity.y = 500;
     this.physics.add.collider(player, platforms);*/
 
-    let map = this.make.tilemap({ key: 'map' });
+    map = this.make.tilemap({ key: 'map' });
     let tileset = map.addTilesetImage('bricks', 'bricks-spritesheet');  // set tileset name
     let layer = map.createLayer('Blocks', tileset, 0, 0);  // set layer name
     layer.setCollisionByProperty({ collidable: true })
-    console.log(map.getLayer(layer).properties)
 
     mario = this.physics.add.sprite(18, 18, 'mario', 'mario-stop.png')
     mario.setPosition(30, 400)
     mario.body.gravity.y = 500;
-    this.physics.add.collider(mario, layer)
+    this.physics.add.collider(mario, layer, checkCollision)
 
     this.anims.create({
         key: 'mario-stop',
@@ -67,7 +67,7 @@ function create ()
     this.anims.create({
         key: 'mario-jump',
         frames: [{ key: 'mario', frame: 'mario-jump.png' }, { key: 'mario', frame: 'mario-fly.png' }],
-        frameRate: 2
+        frameRate: 4
     })
 
     this.anims.create({
@@ -76,31 +76,53 @@ function create ()
     })
 
     this.cameras.main.startFollow(mario, true, 0.05, 0.05)
-
     //mario.anims.play('mario-stop')
     //mario.flipX = true
 }
 
 function update(t, dt) {
+    // movement
     if(cursors.left.isDown) {
         mario.setVelocityX(-moveForce);
-        mario.anims.play('mario-run', true)
         mario.flipX = true
     }
     else if(cursors.right.isDown) {
         mario.setVelocityX(moveForce);
-        mario.anims.play('mario-run', true)
         mario.flipX = false
     }
     else {
         mario.setVelocityX(0);
-        mario.anims.play('mario-stop')
     }
     
     if(cursors.up.isDown && mario.body.onFloor()){
         mario.setVelocityY(-jumpForce);
+    }
+
+    // animation
+    if(cursors.left.isDown && mario.body.onFloor()) {
+        mario.anims.play('mario-run', true)
+    }
+    else if(cursors.right.isDown && mario.body.onFloor()) { 
+        mario.anims.play('mario-run', true)
+    }
+    else if(mario.body.onFloor()) {
+        mario.anims.play('mario-stop')
+    }
+
+    if(cursors.up.isDown && mario.body.onFloor()){ 
         mario.anims.play('mario-jump', true)
     }
 }
 
-    
+function checkCollision(player, obj) {
+    if(obj.properties.breakable && mario.body.onCeiling()){
+        map.removeTile(obj)
+        console.log(obj)
+    }
+
+    if(obj.properties.pointBlock && mario.body.onCeiling()) {
+        // Dodać pokazywanie się punktu nad blokiem
+        console.log("spawn point")
+        obj.properties.pointBlock = false
+    }
+}
