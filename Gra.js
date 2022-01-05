ce81dc7a027868e7517dc2243ac7ea3c13fa08d8
Feekohur <dudadudadudadudada@gmail.com>
2022-01-05 15:58:46 +0100

    Moving Goomba, and Mario ending game

--- Gra.js
+++ Gra.js
@@ -25,13 +25,17 @@ function preload ()
 {
     this.load.atlas('mario', 'mario/mario.png', 'mario/mario.json')
     this.load.tilemapTiledJSON('map', 'map/mapa.json');
-    this.load.image('bricks-spritesheet', 'map/bricks-spritesheet.png');  
+    this.load.image('bricks-spritesheet', 'map/bricks-spritesheet.png');
+    this.load.spritesheet('coin', 'sheets/Coin2.png', {frameWidth: 16, frameHeight: 16});
+    this.load.spritesheet('goomba', 'sheets/Goomba.png', {frameWidth: 18, frameHeight: 18});  
 }
 
 //let player;
 let cursors;
 let mario;
 let map;
+let goombas;
+let gameOver = false;
 
 function create ()
 {
@@ -78,9 +82,64 @@ function create ()
     this.cameras.main.startFollow(mario, true, 0.05, 0.05)
     //mario.anims.play('mario-stop')
     //mario.flipX = true
+
+    goombas = this.physics.add.group({
+        key: 'goomba',
+        repeat: 1,
+        setXY:{x:100, y:400, stepX:515, stepY:-100},
+    });
+
+    this.anims.create({
+        key: 'goomba-run',
+        frames: this.anims.generateFrameNames('goomba', {start: 0, end: 1}),
+        frameRate:8,
+        repeat:-1
+    });
+
+
+
+    this.anims.create({
+        key: 'goomba-dead',
+        frames: this.anims.generateFrameNames('goomba', {frame: 2}),
+        frameRate:8
+    });
+
+    this.anims.create({
+        key: 'goomba-shot',
+        frames: this.anims.generateFrameNames('goomba', {frame: 3}),
+        frameRate:8
+    });
+
+    this.anims.create({
+        key: 'flipping-coin',
+        frames: this.anims.generateFrameNames('coin', {start: 0, end: 3}),
+        frameRate:8
+    });
+
+    Phaser.Actions.Call(goombas.getChildren(), function(goomba) {
+        goomba.minX = goomba.x-25;
+        goomba.maxX = goomba.x+125;
+        goomba.speed = 1;
+        goomba.setGravityY(500);
+      }, this);
+
+    this.physics.add.collider(goombas, layer);
+    this.physics.add.collider(mario, goombas, null, function ()
+    {
+        mario.anims.play('mario-lose');
+        this.physics.pause();
+        gameOver = true;
+        this.cameras.main.stopFollow();
+        //MarioDead(mario.y);
+    }, this);
 }
 
 function update(t, dt) {
+
+    let group_of_goombas = goombas.getChildren();
+    if(gameOver){
+        return;
+    }
     // movement
     if(cursors.left.isDown) {
         mario.setVelocityX(-moveForce);
@@ -111,6 +170,32 @@ function update(t, dt) {
 
     if(cursors.up.isDown && mario.body.onFloor()){ 
         mario.anims.play('mario-jump', true)
+    }
+    //Goombas movement
+    for(let i=0; i<group_of_goombas.length; i++){
+        if(group_of_goombas[i].x>group_of_goombas[i].minX && group_of_goombas[i].x<group_of_goombas[i].maxX){
+            group_of_goombas[i].x+=group_of_goombas[i].speed;
+            group_of_goombas[i].anims.play('goomba-run', true);
+        }
+        else if(group_of_goombas[i].x<=group_of_goombas[i].minX){
+            group_of_goombas[i].speed*=(-1);
+            group_of_goombas[i].x+=group_of_goombas[i].speed;
+            group_of_goombas[i].anims.play('goomba-run', true);
+        }
+        if(group_of_goombas[i].x>=group_of_goombas[i].maxX){
+            group_of_goombas[i].speed*=(-1);
+            group_of_goombas[i].x+=group_of_goombas[i].speed;
+            group_of_goombas[i].anims.play('goomba-run', true);
+        }
+    }
+
+    //Mario losing
+    if(mario.y>550){
+        mario.anims.play('mario-lose');
+        this.physics.pause();
+        gameOver = true;
+        this.cameras.main.stopFollow();
+        //MarioDead(mario.y);
     }
 }
 
